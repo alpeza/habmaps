da52b68323a251403e3920a8acedde2aebb83361
Álvaro Peris <alvaroperis_06@hotmail.com>
2021-04-05 16:17:07 +0200

    wip

--- RPiDockerDeploy/README.md
+++
@@ -1,7 +0,0 @@
-# Habmaps en RPi
-
-1.- Descargamos el instalador en RPi
-
-```bash
-wget 
-```
--- RPiDockerDeploy_wip/install.sh
+++ RPiDockerDeploy_wip/install.sh
@@ -1,10 +1,17 @@
 echo "=> STAGE 1: Installing Docker"
 set -x
 sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 sudo curl -sSL https://get.docker.com | sh
 pi=$(which pip)
 if [ -z $pi  ]; then
   echo "Trying to install pip"
+  sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && sudo python3 get-pip.py
 fi
 sudo pip3 install docker-compose
 docker-compose --version
@@ -17,12 +24,10 @@ echo "=> STAGE 3: Installing Habmaps"
 set -x
 git clone https://github.com/alpeza/habmaps.git
 sudo chmod -R 777 habmaps
-cd habmaps/RPiDockerDeploy
+cd habmaps
+sudo docker build -t habmaps4pi:latest .
+cd RPiDockerDeploy
 cp -r ../mosquitto .
 sudo docker-compose up -d
-sudo docker logs -f habmaps_habmaps_1
+sudo docker-compose logs --tail="all"
 set +x
-
-
-
-
